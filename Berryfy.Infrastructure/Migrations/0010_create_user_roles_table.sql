c96095e6f7f1d43c3a069564525aaba0d158f1c9
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-18 23:59:39 +0300

    Add user_roles table with cascading deletes
    Created user_roles table linking AspNetUsers and AspNetRoles via user_id and role_id. Set composite primary key and enabled cascading deletes on foreign keys.

---
+++ Berryfy.Infrastructure/Migrations/0010_create_user_roles_table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE user_roles
+	(
+		user_id INTEGER NOT NULL REFERENCES "AspNetUsers"("Id") ON DELETE CASCADE, 
+		role_id INTEGER NOT NULL REFERENCES "AspNetRoles"("Id") ON DELETE CASCADE, 
+		PRIMARY KEY (user_id,role_id)
+	);
