d2c313f7836c4dfe48878920f171945824e26553
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-20 22:56:32 +0300

    Refactor ICouponService to use new DTOs
    Updated ICouponService interface to use CouponResponse for responses and CreateCoupon/UpdateCoupon for requests, replacing the old CouponDto, CreateCouponDto, and UpdateCouponDto types. Adjusted using statements to reference the new DTO namespaces.

--- Berryfy.Application/Services/Interfaces/CouponServiceInterfaces/ICouponService.cs
+++ Berryfy.Application/Services/Interfaces/CouponServiceInterfaces/ICouponService.cs
@@ -1,14 +1,15 @@
-﻿using Berryfy.Application.Dtos.CouponDtos;
+﻿using Berryfy.Application.Dtos.CouponDtos.Requests;
+using Berryfy.Application.Dtos.CouponDtos.Responses;
 
 namespace Berryfy.Application.Services.Interfaces.CouponServiceInterfaces
 {
     public interface ICouponService
     {
-        Task<CouponDto> GetByIdAsync(int id);
-        Task<CouponDto> GetByCodeAsync(string code);
-        Task<IEnumerable<CouponDto>> GetAllAsync();
-        Task<CouponDto> CreateAsync(CreateCouponDto couponDto);
-        Task<CouponDto> UpdateAsync(int id, UpdateCouponDto couponDto);
+        Task<CouponResponse> GetByIdAsync(int id);
+        Task<CouponResponse> GetByCodeAsync(string code);
+        Task<IEnumerable<CouponResponse>> GetAllAsync();
+        Task<CouponResponse> CreateAsync(CreateCoupon couponDto);
+        Task<CouponResponse> UpdateAsync(int id, UpdateCoupon couponDto);
         Task<bool> DeleteAsync(int id);
         Task<bool> ExistsByIdAsync(int id);
         Task<bool> ExistsByCodeAsync(string code);
