5d84a85ec306a206a69c955c859730cd78624c5b
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-20 21:38:26 +0300

    Add MapToCategory method to UpdateCategoryRequest
    Added static MapToCategory for mapping UpdateCategoryRequest to Category entity. Included using directive for Berryfy.Domain.Entities.ProductEntities. No code removed.

--- Berryfy.Application/Dtos/CategoryDtos/Requests/UpdateCategoryRequest.cs
+++ Berryfy.Application/Dtos/CategoryDtos/Requests/UpdateCategoryRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace Berryfy.Application.Dtos.CategoryDtos.Requests
+﻿using Berryfy.Domain.Entities.ProductEntities;
+
+namespace Berryfy.Application.Dtos.CategoryDtos.Requests
 {
     public class UpdateCategoryRequest
     {
@@ -6,5 +8,16 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+
+        public static Category MapToCategory(UpdateCategoryRequest request)
+        {
+            return new Category
+            {
+                Id = request.Id,
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl
+            };
+        }
     }
 }
