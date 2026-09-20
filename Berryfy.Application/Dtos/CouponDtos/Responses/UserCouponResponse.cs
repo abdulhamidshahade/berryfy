1d7b9b81ceda68af59d8ccfcece1cfb171716ad5
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-20 22:54:24 +0300

    Add MapFromUserCoupon to UserCouponResponse DTO
    Added static MapFromUserCoupon method for mapping UserCoupon entities to UserCouponResponse DTOs. Included necessary using directive for Berryfy.Domain.Entities.CouponEntities.

--- Berryfy.Application/Dtos/CouponDtos/Responses/UserCouponResponse.cs
+++ Berryfy.Application/Dtos/CouponDtos/Responses/UserCouponResponse.cs
@@ -1,9 +1,21 @@
-﻿namespace Berryfy.Application.Dtos.CouponDtos.Responses
+﻿using Berryfy.Domain.Entities.CouponEntities;
+
+namespace Berryfy.Application.Dtos.CouponDtos.Responses
 {
     public class UserCouponResponse
     {
         public int UserId { get; set; }
         public int CouponId { get; set; }
         public bool IsUsed { get; set; } = false;
+
+        public static UserCouponResponse MapFromUserCoupon(UserCoupon userCoupon)
+        {
+            return new UserCouponResponse
+            {
+                UserId = userCoupon.Id,
+                CouponId = userCoupon.CouponId,
+                IsUsed = userCoupon.IsUsed
+            };
+        }
     }
 }
