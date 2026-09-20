66fe39bfd474e3324d010b03b512ee3fa0051788
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-20 22:53:28 +0300

    Add MapToCoupon method to UpdateCoupon class
    Added using for CouponEntities. Implemented static MapToCoupon to map UpdateCoupon to Coupon entity.

--- Berryfy.Application/Dtos/CouponDtos/Requests/UpdateCoupon.cs
+++ Berryfy.Application/Dtos/CouponDtos/Requests/UpdateCoupon.cs
@@ -1,4 +1,5 @@
 ﻿using Berryfy.Domain.Constants;
+using Berryfy.Domain.Entities.CouponEntities;
 
 namespace Berryfy.Application.Dtos.CouponDtos.Requests
 {
@@ -14,5 +15,21 @@ namespace Berryfy.Application.Dtos.CouponDtos.Requests
         public CouponType Type { get; set; }
         public decimal Value { get; set; }
         public bool IsForNewUsersOnly { get; set; }
+
+        public static Coupon MapToCoupon(UpdateCoupon request)
+        {
+            return new Coupon
+            {
+                Id = request.Id,
+                Code = request.Code,
+                DiscountAmount = request.DiscountAmount,
+                MinimumOrderAmount = request.MinimumOrderAmount,
+                Description = request.Description,
+                IsActive = request.IsActive,
+                Type = request.Type,
+                Value = request.Value,
+                IsForNewUsersOnly = request.IsForNewUsersOnly
+            };
+        }
     }
 }
