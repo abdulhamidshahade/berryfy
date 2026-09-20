4b2d761265d5076e543027ba09570d46b3f3c066
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-20 21:15:29 +0300

    Add MapToCategory method to CreateCategoryRequest
    Added static MapToCategory to map request to Category entity.
    Set Name, Description, ImageUrl, CreatedAt properties.
    Added using for Berryfy.Domain.Entities.ProductEntities.

--- Berryfy.Application/Dtos/CategoryDtos/Requests/CreateCategoryRequest.cs
+++ Berryfy.Application/Dtos/CategoryDtos/Requests/CreateCategoryRequest.cs
@@ -1,9 +1,22 @@
-﻿namespace Berryfy.Application.Dtos.CategoryDtos.Requests
+﻿using Berryfy.Domain.Entities.ProductEntities;
+
+namespace Berryfy.Application.Dtos.CategoryDtos.Requests
 {
     public class CreateCategoryRequest
     {
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+
+        public static Category MapToCategory(CreateCategoryRequest request)
+        {
+            return new Category
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
     }
 }
