ad2dfa9cedc7bc04791832350f32e366af3a5816
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-20 22:53:57 +0300

    Add static mapping methods to CouponResponse
    Added static methods to CouponResponse for mapping between Coupon and CouponResponse objects, including single and collection conversions. Included necessary using directive for Berryfy.Domain.Entities.CouponEntities.

--- Berryfy.Application/Dtos/CouponDtos/Responses/CouponResponse.cs
+++ Berryfy.Application/Dtos/CouponDtos/Responses/CouponResponse.cs
@@ -1,4 +1,5 @@
 ﻿using Berryfy.Domain.Constants;
+using Berryfy.Domain.Entities.CouponEntities;
 
 namespace Berryfy.Application.Dtos.CouponDtos.Responses
 {
@@ -14,5 +15,61 @@ namespace Berryfy.Application.Dtos.CouponDtos.Responses
         public CouponType Type { get; set; }
         public decimal Value { get; set; }
         public bool IsForNewUsersOnly { get; set; }
+
+        public static CouponResponse MapFromCoupon(Coupon coupon)
+        {
+            return new CouponResponse
+            {
+                Id = coupon.Id,
+                Code = coupon.Code,
+                DiscountAmount = coupon.DiscountAmount,
+                MinimumOrderAmount = coupon.MinimumOrderAmount,
+                Description = coupon.Description,
+                IsActive = coupon.IsActive,
+                Type = coupon.Type,
+                Value = coupon.Value,
+                IsForNewUsersOnly = coupon.IsForNewUsersOnly
+            };
+        }
+
+        public static Coupon MapToCoupon(CouponResponse couponResponse)
+        {
+            return new Coupon
+            {
+                Id = couponResponse.Id,
+                Code = couponResponse.Code,
+                DiscountAmount = couponResponse.DiscountAmount,
+                MinimumOrderAmount = couponResponse.MinimumOrderAmount,
+                Description = couponResponse.Description,
+                IsActive = couponResponse.IsActive,
+                Type = couponResponse.Type,
+                Value = couponResponse.Value,
+                IsForNewUsersOnly = couponResponse.IsForNewUsersOnly
+            };
+        }
+
+        public static List<Coupon> MapToCoupon(IEnumerable<CouponResponse> couponResponses)
+        {
+            List<Coupon> coupons = new List<Coupon>();
+
+            foreach(CouponResponse couponResponse in couponResponses)
+            {
+                coupons.Add(MapToCoupon(couponResponse));
+            }
+
+            return coupons;
+        }
+
+        public static List<CouponResponse> MapFromCoupon(IEnumerable<Coupon> coupons)
+        {
+            List<CouponResponse> couponResponses = new List<CouponResponse>();
+
+            foreach(Coupon coupon in coupons)
+            {
+                couponResponses.Add(MapFromCoupon(coupon));
+            }
+
+            return couponResponses;
+        }
     }
 }
