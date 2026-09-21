3459d17fe22e4d1355c6797069807c4087a14c14
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-21 11:31:05 +0300

    Refactor IProductCategoryService to replace ProductDto with ProductResponse in method signatures

--- Berryfy.Application/Services/Interfaces/ProductServiceInterfaces/IProductCategoryService.cs
+++ Berryfy.Application/Services/Interfaces/ProductServiceInterfaces/IProductCategoryService.cs
@@ -1,10 +1,10 @@
-﻿using Berryfy.Application.Dtos.ProductDtos;
+﻿using Berryfy.Application.Dtos.ProductDtos.Responses;
 
 namespace Berryfy.Application.Services.Interfaces.ProductServiceInterfaces
 {
     public interface IProductCategoryService
     {
-        Task<bool> AddProductCategoryAsync(ProductDto Product, List<int> categories);
-        Task<bool> UpdateProductCategoryAsync(ProductDto product, List<int> categories);
+        Task<bool> AddProductCategoryAsync(ProductResponse Product, List<int> categories);
+        Task<bool> UpdateProductCategoryAsync(ProductResponse product, List<int> categories);
     }
 }
