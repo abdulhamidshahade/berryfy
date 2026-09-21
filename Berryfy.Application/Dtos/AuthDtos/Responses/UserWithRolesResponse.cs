e12a315e6247462565cdcdc296c4e518191af558
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-21 11:48:30 +0300

    Refactor UserWithRolesResponse to include necessary using directives and fix syntax errors

--- Berryfy.Application/Dtos/AuthDtos/Responses/UserWithRolesResponse.cs
+++ Berryfy.Application/Dtos/AuthDtos/Responses/UserWithRolesResponse.cs
@@ -1,4 +1,7 @@
-﻿namespace Berryfy.Application.Dtos.AuthDtos.Responses
+﻿using Berryfy.Application.Services.Concretes.AuthServiceConcretes;
+using Berryfy.Domain.Entities.AuthEntities;
+
+namespace Berryfy.Application.Dtos.AuthDtos.Responses
 {
     public class UserWithRolesResponse
     {
@@ -10,6 +13,23 @@
         public List<string> Roles { get; set; }
         public bool EmailConfirmed { get; set; }
         public DateTimeOffset? LockoutEnd { get; set; }
-        public int AccessFailedCount { get; set; }
+        public int AccessFailedCount { get; set; }]
+
+
+        public static UserWithRolesResponse MapFromUser(User user)
+        {
+            return new UserWithRolesResponse
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                EmailConfirmed = user.EmailConfirmed,
+                LockoutEnd = user.LockoutEnd,
+                AccessFailedCount = user.AccessFailedCount,
+                Roles = user.Roles.Select(r => new RoleManagementService(). r.RoleId).ToList()
+            };
+        }
     }
 }
