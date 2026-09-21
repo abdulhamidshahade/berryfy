e4b4c9499b5df65be7d7467c1bcdcc30529d4518
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-21 11:48:15 +0300

    Refactor RoleResponse to include mapping methods for Role entity and adjust using directives

--- Berryfy.Application/Dtos/AuthDtos/Responses/RoleResponse.cs
+++ Berryfy.Application/Dtos/AuthDtos/Responses/RoleResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace Berryfy.Application.Dtos.AuthDtos.Responses
+﻿using Berryfy.Domain.Entities.AuthEntities;
+
+namespace Berryfy.Application.Dtos.AuthDtos.Responses
 {
     public class RoleResponse
     {
@@ -6,5 +8,28 @@
         public string Name { get; set; }
         public string concurrencyStamp { get; set; }
         public string normalizedName { get; set; }
+
+        public static RoleResponse MapFromRole(Role role)
+        {
+            return new RoleResponse
+            {
+                Id = role.Id,
+                Name = role.Name,
+                concurrencyStamp = role.ConcurrencyStamp,
+                normalizedName = role.NormalizedName
+            };
+        }
+
+        public static List<RoleResponse> MapFromRole(IEnumerable<Role> roles)
+        {
+            List<RoleResponse> roleResponses = new List<RoleResponse>();
+
+            foreach(Role role in roles)
+            {
+                roleResponses.Add(MapFromRole(role));
+            }
+
+            return roleResponses;
+        }
     }
 }
