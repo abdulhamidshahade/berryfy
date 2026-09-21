9738b4f8098afda4d52dbaefce4f6d911de44a56
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-21 12:52:20 +0300

    Refactor IUserCheckoutInfoService to use UserCheckoutInfoResponse DTO

--- Berryfy.Application/Services/Interfaces/CheckoutServiceInterfaces/IUserCheckoutInfoService.cs
+++ Berryfy.Application/Services/Interfaces/CheckoutServiceInterfaces/IUserCheckoutInfoService.cs
@@ -1,12 +1,13 @@
-using Berryfy.Application.Dtos.CheckoutDtos;
+using Berryfy.Application.Dtos.CheckoutDtos.Requests;
+using Berryfy.Application.Dtos.CheckoutDtos.Responses;
 
 namespace Berryfy.Application.Services.Interfaces.CheckoutServiceInterfaces
 {
     public interface IUserCheckoutInfoService
     {
-        Task<UserCheckoutInfoDto?> GetCheckoutInfoAsync(int userId);
-        Task<UserCheckoutInfoDto> SaveCheckoutInfoAsync(int userId, SaveCheckoutInfoDto dto);
-        Task<UserCheckoutInfoDto> SavePaymentBillingInfoAsync(int userId, SavePaymentBillingDto dto);
+        Task<UserCheckoutInfoResponse?> GetCheckoutInfoAsync(int userId);
+        Task<UserCheckoutInfoResponse> SaveCheckoutInfoAsync(int userId, SaveCheckoutInfo dto);
+        Task<UserCheckoutInfoResponse> SavePaymentBillingInfoAsync(int userId, SavePaymentBilling dto);
         Task<bool> DeleteCheckoutInfoAsync(int userId);
     }
 }
