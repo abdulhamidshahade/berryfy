bbe1d054252773effa2a52fe85a4244a001a871f
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-22 16:36:44 +0300

    Refactor UserRole class and optimize serialization
    Removed unused `using` directives to clean up the code. Added `using System.Text.Json.Serialization` to support JSON-related functionality. Applied `[JsonIgnore]` attributes to `User` and `Role` properties in the `UserRole` class to exclude them from JSON serialization.

--- Berryfy.Domain/Entities/AuthEntities/UserRole.cs
+++ Berryfy.Domain/Entities/AuthEntities/UserRole.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace Berryfy.Domain.Entities.AuthEntities
 {
@@ -10,5 +6,7 @@ namespace Berryfy.Domain.Entities.AuthEntities
     {
         public int UserId { get; set; }
         public int RoleId { get; set; }
+        [JsonIgnore] public User User { get; set; }
+        [JsonIgnore] public Role Role { get; set; }
     }
 }
