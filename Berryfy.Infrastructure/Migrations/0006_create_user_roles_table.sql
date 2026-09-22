de740c53f37efaa50e625c1f9efa81d335a6098c
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-22 16:21:22 +0300

    Update foreign key references in user_roles table
    Updated the foreign key references for the `user_id` and `role_id`
    columns in the `user_roles` table to point to the `id` columns in
    the `users` and `roles` tables, respectively, instead of the
    `AspNetUsers` and `AspNetRoles` tables. The `ON DELETE CASCADE`
    behavior remains unchanged. This aligns the schema with updated
    table naming conventions.

--- Berryfy.Infrastructure/Migrations/0006_create_user_roles_table.sql
+++ Berryfy.Infrastructure/Migrations/0006_create_user_roles_table.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE user_roles
 	(
-		user_id INTEGER NOT NULL REFERENCES "AspNetUsers"("Id") ON DELETE CASCADE, 
-		role_id INTEGER NOT NULL REFERENCES "AspNetRoles"("Id") ON DELETE CASCADE, 
+		user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE, 
+		role_id INTEGER NOT NULL REFERENCES roles(id) ON DELETE CASCADE, 
 		PRIMARY KEY (user_id,role_id)
 	);
