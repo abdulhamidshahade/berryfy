18ccb6a550d90b111cd59e4393429ddad34e0ffe
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-23 22:10:45 +0300

    Add InfrastructureResponse<T> class for generic responses
    A new generic class `InfrastructureResponse<T>` was introduced in the `Berryfy.Application.Dtos` namespace. This class provides a standardized structure for API responses, including properties for success status (`IsSuccess`), a descriptive message (`Message`), and a value of type `T` (`Value`).
    
    Additionally, necessary `using` directives were added to support the implementation.

---
+++ Berryfy.Application/Dtos/InfrastructureResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berryfy.Application.Dtos
+{
+    public class InfrastructureResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Value { get; set; }
+    }
+}
