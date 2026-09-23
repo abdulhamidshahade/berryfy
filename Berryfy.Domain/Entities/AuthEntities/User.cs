92a8fe61d29f04bfab1223957f0edef844242ae2
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-23 23:09:45 +0300

    Refactor User class property initialization
    Removed default values from properties and collections in the `User` class within the `Berryfy.Domain.Entities.AuthEntities` namespace. This change simplifies initialization and allows for greater flexibility in setting property values at runtime.

--- Berryfy.Domain/Entities/AuthEntities/User.cs
+++ Berryfy.Domain/Entities/AuthEntities/User.cs
@@ -10,17 +10,17 @@ namespace Berryfy.Domain.Entities.AuthEntities
     public class User
     {
         public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string UserName { get; set; } = string.Empty;
-        public string NormalizedUserName { get; set; } = string.Empty;
-        public string NormalizedEmail { get; set; } = string.Empty;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string NormalizedEmail { get; set; }
         public string? PhoneNumber { get; set; }
         public bool EmailConfirmed { get; set; }
-        public string Email { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-        public string SecurityStamp { get; set; } = Guid.NewGuid().ToString();
-        public string ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string ConcurrencyStamp { get; set; }
         public bool PhoneNumberConfirmed { get; set; }
         public bool TwoFactorEnabled { get; set; }
         public DateTime? LockoutEnd { get; set; }
@@ -32,11 +32,11 @@ namespace Berryfy.Domain.Entities.AuthEntities
         public DateTime? PasswordResetCodeExpiry { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime? RefreshTokenExpiry { get; set; }
-        [JsonIgnore] public List<UserCoupon> UserCoupons { get; set; } = new();
-        [JsonIgnore] public List<Cart> Cart { get; set; } = new();
-        [JsonIgnore] public List<Order> Orders { get; set; } = new();
-        [JsonIgnore] public List<UserRole> Roles { get; set; } = new();
-        [JsonIgnore] public List<Wishlist> Wishlists { get; set; } = new();
-        [JsonIgnore] public List<Payment> Payments { get; set; } = new();
+        [JsonIgnore] public List<UserCoupon> UserCoupons { get; set; }
+        [JsonIgnore] public List<Cart> Cart { get; set; } 
+        [JsonIgnore] public List<Order> Orders { get; set; }
+        [JsonIgnore] public List<UserRole> Roles { get; set; } 
+        [JsonIgnore] public List<Wishlist> Wishlists { get; set; }
+        [JsonIgnore] public List<Payment> Payments { get; set; }
     }
 }
