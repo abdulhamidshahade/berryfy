4b7e4ea26efac3b2361783ebaa995851fb542c1d
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-23 22:10:35 +0300

    Add generic ApplicationResponse<T> class for API responses
    Introduced a new `ApplicationResponse<T>` class in the
    `Berryfy.Application.Dtos` namespace to standardize API responses.
    The class includes `IsSuccess`, `Value`, and `ErrorMessage`
    properties to represent the success status, result, and error
    details of an operation. Added a `using` directive for
    `Berryfy.Application.Constants`.

---
+++ Berryfy.Application/Dtos/ApplicationResponse.cs
@@ -0,0 +1,11 @@
+﻿using Berryfy.Application.Constants;
+
+namespace Berryfy.Application.Dtos
+{
+    public class ApplicationResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public T? Value { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
