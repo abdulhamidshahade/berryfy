c6d2cb74be2f5041178425930b04c8eca2b43b4a
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-23 22:09:57 +0300

    Rename ResponseDto<T> to ApiResponse<T>
    Renamed the class `ResponseDto<T>` to `ApiResponse<T>` in the
    `Berryfy.Application.Dtos` namespace to improve clarity and
    better reflect its purpose in the codebase.

--- Berryfy.Application/Dtos/ApiResponse.cs
+++ Berryfy.Application/Dtos/ApiResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace Berryfy.Application.Dtos
 {
-    public class ResponseDto<T>
+    public class ApiResponse<T>
     {
         public bool IsSuccess { get; set; }
         public int StatusCode { get; set; }
