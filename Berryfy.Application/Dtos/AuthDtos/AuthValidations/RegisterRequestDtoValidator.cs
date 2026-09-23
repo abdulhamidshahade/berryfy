8c23835a780eb26030649125bce339c19fc86183
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-23 12:34:36 +0300

    Increase max length for FirstName and LastName to 100
    Updated validation rules in `RegisterRequestDtoValidator` to allow a maximum length of 100 characters for both `FirstName` and `LastName`. This change provides greater flexibility for users with longer names.

--- Berryfy.Application/Dtos/AuthDtos/AuthValidations/RegisterRequestDtoValidator.cs
+++ Berryfy.Application/Dtos/AuthDtos/AuthValidations/RegisterRequestDtoValidator.cs
@@ -12,14 +12,14 @@ namespace Berryfy.Application.Dtos.AuthDtos.AuthValidations
                 .NotEmpty().WithMessage("First name is required.")
                 .Matches(@"^[\p{L}\s'-]+$").WithMessage("First name must contain only letters.")
                 .MinimumLength(1).WithMessage("First name must be at least 2 characters long.")
-                .MaximumLength(100).WithMessage("First name must be at most 50 characters long.");
+                .MaximumLength(100).WithMessage("First name must be at most 100 characters long.");
 
             RuleFor(fn => fn.LastName)
                 .NotNull().WithMessage("Last name is required.")
                 .NotEmpty().WithMessage("Last name is required.")
                 .Matches(@"^[\p{L}\s'-]+$").WithMessage("Last name must contain only letters.")
                 .MinimumLength(1).WithMessage("Last name must be at least 2 characters long.")
-                .MaximumLength(100).WithMessage("Last name must be at most 50 characters long.");
+                .MaximumLength(100).WithMessage("Last name must be at most 100 characters long.");
 
             RuleFor(fn => fn.UserName)
                 .NotNull().WithMessage("Username is required.")
