cc93a349d33954aadaa1a9f77338623d6a2859b7
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-24 20:53:16 +0300

    Add UpdateRoleRequest class for role update functionality
    A new `UpdateRoleRequest` class was introduced in the
    `Berryfy.Application.Dtos.AuthDtos.Requests` namespace. This class
    includes two properties: `oldRoleName` and `newRoleName`, which
    represent the current role name and the updated role name, respectively.
    
    Additionally, necessary `using` directives were added to the file
    to support the new class implementation.

---
+++ Berryfy.Application/Dtos/AuthDtos/Requests/UpdateRoleRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berryfy.Application.Dtos.AuthDtos.Requests
+{
+    public class UpdateRoleRequest
+    {
+        public string oldRoleName { get; set; }
+        public string newRoleName { get; set; }
+    }
+}
