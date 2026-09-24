171d1adbab4236b8d43356074761444cec004450
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-24 15:36:28 +0300

    Refactor namespace and add InfrastructureResponse class
    The namespace of the `InfrastructureResponse` class was updated
    from `Berryfy.Application.Dtos` to `Berryfy.Domain.Entities`.
    
    Added the `InfrastructureResponse<T>` generic class with the
    following properties:
    - `IsSuccess` (bool): Indicates the success status.
    - `Message` (string): Provides additional context or feedback.

--- Berryfy.Domain/Entities/InfrastructureResponse.cs
+++ Berryfy.Domain/Entities/InfrastructureResponse.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Berryfy.Application.Dtos
+namespace Berryfy.Domain.Entities
 {
     public class InfrastructureResponse<T>
     {
