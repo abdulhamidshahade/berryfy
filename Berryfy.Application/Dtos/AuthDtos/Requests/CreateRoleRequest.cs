0c36d737b91fc696f266e3ca6fd1c83a413129f1
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-24 20:52:43 +0300

    Add CreateRoleRequest class to AuthDtos namespace
    Introduce the `CreateRoleRequest` class in the `Berryfy.Application.Dtos.AuthDtos.Requests` namespace. This class includes a `roleName` property of type `string` with `get` and `set` accessors. Added necessary `using` directives for required namespaces.

---
+++ Berryfy.Application/Dtos/AuthDtos/Requests/CreateRoleRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berryfy.Application.Dtos.AuthDtos.Requests
+{
+    public class CreateRoleRequest
+    {
+        public string roleName { get; set; }
+    }
+}
