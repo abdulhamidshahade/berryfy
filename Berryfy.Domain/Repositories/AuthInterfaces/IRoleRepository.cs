d01e544fd017e6eb1b0c43aa94cda416aa975d32
AbdulhamidShahade <abdulhamidshahade1412@gmail.com>
2026-09-24 19:33:12 +0300

    Refactor IRoleRepository return types
    Updated all methods in the `IRoleRepository` interface to use the `InfrastructureResponse<T>` wrapper for return types, replacing plain types like `bool`, `Role`, and `List<Role>`. This standardizes responses to include additional metadata alongside the data.
    
    Added necessary `using` directives for `Berryfy.Domain.Entities` and `Berryfy.Domain.Entities.AuthEntities` to support the updated interface.

--- Berryfy.Domain/Repositories/AuthInterfaces/IRoleRepository.cs
+++ Berryfy.Domain/Repositories/AuthInterfaces/IRoleRepository.cs
@@ -1,21 +1,22 @@
+using Berryfy.Domain.Entities;
 using Berryfy.Domain.Entities.AuthEntities;
 
 namespace Berryfy.Domain.Repositories.AuthInterfaces
 {
     public interface IRoleRepository
     {
-        Task<bool> RoleExistsAsync(string roleName);
-        Task<Role?> GetByNameAsync(string roleName);
-        Task<Role> CreateAsync(Role role);
-        Task<bool> DeleteAsync(string roleName);
-        Task<bool> UpdateAsync(string oldRoleName, string newRoleName);
-        Task<List<Role>> GetAllAsync();
-        Task<bool> AssignRoleToUserAsync(int userId, string roleName);
-        Task<bool> RemoveRoleFromUserAsync(int userId, string roleName);
-        Task<bool> IsUserInRoleAsync(int userId, string roleName);
-        Task<List<string>> GetUserRolesAsync(int userId);
-        Task<List<User>> GetUsersInRoleAsync(string roleName);
-        Task<List<(User User, List<string> Roles)>> GetAllUsersWithRolesAsync();
-        Task<(int TotalRoles, int TotalUsers, int UsersWithRoles, int UsersWithoutRoles)> GetRoleStatsAsync();
+        Task<InfrastructureResponse<bool>> RoleExistsAsync(string roleName);
+        Task<InfrastructureResponse<Role?>> GetByNameAsync(string roleName);
+        Task<InfrastructureResponse<Role>> CreateAsync(Role role);
+        Task<InfrastructureResponse<bool>> DeleteAsync(string roleName);
+        Task<InfrastructureResponse<bool>> UpdateAsync(string oldRoleName, string newRoleName);
+        Task<InfrastructureResponse<List<Role>>> GetAllAsync();
+        Task<InfrastructureResponse<bool>> AssignRoleToUserAsync(int userId, string roleName);
+        Task<InfrastructureResponse<bool>> RemoveRoleFromUserAsync(int userId, string roleName);
+        Task<InfrastructureResponse<bool>> IsUserInRoleAsync(int userId, string roleName);
+        Task<InfrastructureResponse<List<string>>> GetUserRolesAsync(int userId);
+        Task<InfrastructureResponse<List<User>>> GetUsersInRoleAsync(string roleName);
+        Task<InfrastructureResponse<List<(User User, List<string> Roles)>>> GetAllUsersWithRolesAsync();
+        Task<InfrastructureResponse<(int TotalRoles, int TotalUsers, int UsersWithRoles, int UsersWithoutRoles)>> GetRoleStatsAsync();
     }
 }
